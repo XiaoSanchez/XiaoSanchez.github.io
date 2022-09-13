e7728396c891997baf8d0b17dafc2b71a295d7b2
Shawn C <79479480+XiaoSanchez@users.noreply.github.com>
2022-09-13 10:47:33 -0400

    v3.3

---
+++ html/Private/js/config.js
@@ -0,0 +1,54 @@
+/*
+ * @Author: Shawn C 111708216+guochengcai@users.noreply.github.com
+ * @Date: 2020-02-08 22:06:08
+ * @LastEditors: Shawn C 111708216+guochengcai@users.noreply.github.com
+ * @LastEditTime: 2022-08-28 17:56:58
+ * @FilePath: /动态时钟/js/config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+
+
+参数配置说明
+
+language_type[语言类型]:
+    0:阿拉伯数字
+    1:简体
+    2:繁体
+    3:英文
+
+font_color[时钟文字颜色]
+
+pointer_color[时钟指针文字颜色]
+
+sound[背景音乐]
+    0:关闭
+    1:开启
+sound_name[背景音乐名]
+    将想要的音乐文件放到sound文件夹下,并且将该参数改为音乐文件名(带后缀)
+
+background_style[背景样式]
+    0:图片
+    1:视频
+    2:纯色
+background_picture[背景图片]
+    将想要的背景图片文件放到images文件夹下,并且将该参数改为背景图片名(带后缀)
+background_video[背景视频]
+    将想要的背景视频文件放到video文件夹下,并且将该参数改为背景视频名(带后缀)
+background_color[背景颜色]
+    填入颜色值
+
+*/
+
+
+var config={
+
+    'language_type':2,
+    'font_color':'#7F7F7F',
+    // 'pointer_color':'#000000',
+
+    'background_style':1,
+    'background_picture':'bg.jpg',
+    'background_video':'3.mp4',
+    'background_cozlor':'#000000',
+}
