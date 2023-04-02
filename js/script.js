5304c04d607d7a1338537ac8b4e8c614258b7f3d
Shawn C <79479480+XiaoSanchez@users.noreply.github.com>
2023-04-01 20:59:35 -0400

    v5.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+/*
+ * @Description: 
+ * @Logo:                                                               ▄   ▄
+ * ░██████╗██╗░░██╗░█████╗░░██╗░░░░░░░██╗███╗░░██╗  ░█████╗░░█████╗░██╗ █▀█▀█
+ * ██╔════╝██║░░██║██╔══██╗░██║░░██╗░░██║████╗░██║  ██╔══██╗██╔══██╗██║ █▄█▄█
+ * ╚█████╗░███████║███████║░╚██╗████╗██╔╝██╔██╗██║  ██║░░╚═╝███████║██║ ███  ▄▄
+ * ░╚═══██╗██╔══██║██╔══██║░░████╔═████║░██║╚████║  ██║░░██╗██╔══██║██║ ████▐█ █
+ * ██████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░██║░╚███║  ╚█████╔╝██║░░██║██║ ████   █
+ * ╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝░░╚══╝  ░╚════╝░╚═╝░░╚═╝╚═╝ ▀▀▀▀▀▀▀
+ * @Author: Shawn C
+ * Copyright (c) 2023 by Shawn C., All Rights Reserved. 
+ */
 (function($) { "use strict";
 
 	$(function() {
@@ -59,56 +71,3 @@
 		window.location.hash = target;
 	});
 });
-
-// GitHub API endpoint for fetching repository data
-const API_URL = "https://api.github.com/repos/";
-
-// Repositories to display
-const repos = [
-	"https://github.com/XiaoSanchez/Facial_Expression_Recognition",
-	"https://github.com/XiaoSanchez/Bootstrap_C-Based_Compiler",
-	"https://github.com/XiaoSanchez/Car_Dealership_Reviews",
-];
-
-// Function to fetch repository data from GitHub API
-async function fetchRepoData(repoUrl) {
-	const repoName = repoUrl.split("/").slice(-2).join("/");
-	const response = await fetch(API_URL + repoName);
-	const repoData = await response.json();
-	return repoData;
-}
-
-// Function to generate HTML content for a repository
-function generateRepoHtml(repoData) {
-	const repoUrl = repoData.html_url;
-	const repoName = repoData.name;
-	const repoDescription = repoData.description;
-	const repoStars = repoData.stargazers_count;
-	const repoForks = repoData.forks_count;
-	const repoLanguage = repoData.language;
-
-	const repoHtml = `
-	<div class="repo-card" style="height:220px; width: 40%; padding: 10px;">
-		<a href="${repoUrl}">${repoName}</a>
-		<p>${repoDescription}</p>
-		<ul>
-		<p>Stars: ${repoStars}&nbsp;&nbsp;&nbsp;&nbsp;Forks: ${repoForks}</p>
-		<p>Language: ${repoLanguage}</p>
-		</ul>
-	</div>
-	`;
-	return repoHtml;
-}
-
-// Function to display repositories on the page
-async function displayRepos() {
-	const repoContainer = document.getElementById("repo-container");
-	for (const repoUrl of repos) {
-	const repoData = await fetchRepoData(repoUrl);
-	const repoHtml = generateRepoHtml(repoData);
-	repoContainer.insertAdjacentHTML("beforeend", repoHtml);
-	}
-}
-
-// Call the displayRepos function to start displaying repositories
-displayRepos();
