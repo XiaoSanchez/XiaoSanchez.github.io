6e98cb8cdb46702e9edd9a7808a2133c2ece21e4
Shawn C <79479480+XiaoSanchez@users.noreply.github.com>
2023-08-03 13:04:34 -0400

    v5.4

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,55 +1,72 @@
-const button = document.querySelector( 'button' );
+const button = document.querySelector('button');
+const usernameInput = document.querySelector('input[name="username"]');
+const passwordInput = document.querySelector('input[name="password"]');
 
-const distanceBetween = ( p1x, p1y, p2x, p2y ) => {
-  const dx = p1x-p2x;
-  const dy = p1y-p2y;
-  return Math.sqrt( dx*dx + dy*dy );
+const distanceBetween = (p1x, p1y, p2x, p2y) => {
+  const dx = p1x - p2x;
+  const dy = p1y - p2y;
+  return Math.sqrt(dx * dx + dy * dy);
 };
 
-document.addEventListener( 'mousemove', event => {
-  if (document.querySelector('input[name="password"]').value &&
-    document.querySelector('input[name="username"]').value) {
+document.addEventListener('mousemove', (event) => {
+  if (passwordInput.value && usernameInput.value) {
     return;
   }
-  
-  const radius = Math.max( button.offsetWidth*0.75, button.offsetHeight*0.75, 100 );
 
-  const bx = button.parentNode.offsetLeft + button.offsetLeft + button.offsetWidth/2;
-  const by = button.parentNode.offsetTop + button.offsetTop + button.offsetHeight/2;
+  const radius = Math.max(button.offsetWidth * 0.75, button.offsetHeight * 0.75, 100);
 
-  const dist = distanceBetween( event.clientX, event.clientY, bx, by );
-  const angle = Math.atan2( event.clientY - by, event.clientX - bx );
+  const bx = button.parentNode.offsetLeft + button.offsetLeft + button.offsetWidth / 2;
+  const by = button.parentNode.offsetTop + button.offsetTop + button.offsetHeight / 2;
 
-  const ox = -1 * Math.cos( angle ) * Math.max( ( radius - dist ), 0 );
-  const oy = -1 * Math.sin( angle ) * Math.max( ( radius - dist ), 0 );
+  const dist = distanceBetween(event.clientX, event.clientY, bx, by);
+  const angle = Math.atan2(event.clientY - by, event.clientX - bx);
+
+  const ox = -1 * Math.cos(angle) * Math.max(radius - dist, 0);
+  const oy = -1 * Math.sin(angle) * Math.max(radius - dist, 0);
 
   const rx = oy / 2;
   const ry = -ox / 2;
 
   button.style.transition = `all 0.1s ease`;
   button.style.transform = `translate(${ox}px, ${oy}px) rotateX(${rx}deg) rotateY(${ry}deg)`;
-  button.style.boxShadow = `0px ${Math.abs(oy)}px ${Math.abs(oy)/radius*40}px rgba(0,0,0,0.15)`;
-} );
+  button.style.boxShadow = `0px ${Math.abs(oy)}px ${Math.abs(oy) / radius * 40}px rgba(0,0,0,0.15)`;
+});
 
-const nocheat = () => button.textContent = 'Nice Try~';
-const notouch = () => button.textContent = 'Brilliant!';
+const nocheat = () => (button.textContent = 'Nice Try~');
+const notouch = () => (button.textContent = 'Brilliant!');
 
-button.addEventListener( 'click', () => button.textContent = 'Here you go!');
-button.addEventListener( 'keydown', event => { event.preventDefault(); nocheat(); } );
+button.addEventListener('keydown', (event) => {
+  event.preventDefault();
+  nocheat();
+});
 button.click = nocheat;
 
-if( navigator.userAgent.match( /Android|iPhone|iPad|iPod/i ) ) notouch();
-window.addEventListener( 'touchstart', notouch );
+if (navigator.userAgent.match(/Android|iPhone|iPad|iPod/i)) notouch();
+window.addEventListener('touchstart', notouch);
 
 function changeImage() {
-  const imageElement = document.querySelector("img");
-  imageElement.src = "../img/2.png";
+  const imageElement = document.querySelector('img');
+  imageElement.src = '../img/2.png';
 }
 
 function changeBackImage() {
-  const imageElement = document.querySelector("img");
-  imageElement.src = "../img/1.png";
+  const imageElement = document.querySelector('img');
+  imageElement.src = '../img/1.png';
 }
 
-const passwordInput = document.getElementById("password");
-passwordInput.addEventListener("blur", changeBackImage);
+const passwordInput1 = document.getElementById('password');
+passwordInput1.addEventListener('blur', changeBackImage);
+
+button.addEventListener('click', () => {
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  const correctUsername = 'admin';
+  const correctPassword = '0000';
+
+  if (username === correctUsername && password === correctPassword) {
+    window.close();
+  } else {
+    window.alert('Incorrect username or password.');
+  }
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -42,7 +42,7 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
+  on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
@@ -51,7 +51,7 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '#navbar .nav-link', function(e) {
+  on('click', '#navbar .nav-link', function (e) {
     let section = select(this.hash)
     if (section) {
       e.preventDefault()
@@ -84,7 +84,7 @@
 
       if (!header.classList.contains('header-top')) {
         header.classList.add('header-top')
-        setTimeout(function() {
+        setTimeout(function () {
           sections.forEach((item) => {
             item.classList.remove('section-show')
           })
@@ -123,7 +123,7 @@
           }
         })
 
-        setTimeout(function() {
+        setTimeout(function () {
           initial_nav.classList.add('section-show')
         }, 350);
 
@@ -140,7 +140,7 @@
     new Waypoint({
       element: skilsContent,
       offset: '80%',
-      handler: function(direction) {
+      handler: function (direction) {
         let progress = select('.progress .progress-bar', true);
         progress.forEach((el) => {
           el.style.width = el.getAttribute('aria-valuenow') + '%'
@@ -191,9 +191,9 @@
 
       let portfolioFilters = select('#portfolio-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#portfolio-flters li', function (e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
+        portfolioFilters.forEach(function (el) {
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
